be4de281af51b88a5b6e07ab0c3abe2f069eea9f
rani1029 <ranig1029@gmail.com>
2021-08-05 23:44:53 +0530

    checkprime num added

---
+++ CheckPrime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CheckPrime
+{
+    class Prime
+    {
+        //int Flag=0;
+        static void Main(string[] args)
+        {
+            int Flag = 0;
+            Console.WriteLine("enter the number to check");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i < num; i++)
+            {
+
+
+                if (num % i == 0)
+                {
+                    Console.WriteLine("it is not prime number");
+                    Flag++;
+                    break;
+                }
+            }
+
+            if (Flag == 0)
+            {
+                Console.WriteLine("it is prime number");
+
+            }
+        }
+    }
+}
