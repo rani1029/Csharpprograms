2e196ca9c7928c8af036a1e51b9731d29478dee8
rani1029 <ranig1029@gmail.com>
2021-08-06 01:27:21 +0530

    PowerOfTwo program added

---
+++ PowerOfTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of power of two");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(" table is ");
+            for (int i = 1; i <= n; i++)
+            {
+                double p = Math.Pow(2, i);
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
