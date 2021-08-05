1a004315d9c4e99adb445cb658bb112d545d46ad
rani1029 <ranig1029@gmail.com>
2021-08-05 16:23:50 +0530

    remainder program added

---
+++ CalculateRemainder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculateRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the dividend");
+            int dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the divisor");
+            int divisor = int.Parse(Console.ReadLine());
+            int remainder = dividend % divisor;
+            int quotient = dividend / divisor;
+
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("remainder is " + remainder);
+
+
+
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
