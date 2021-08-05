edc18b90f7ea76c0b17c1d4b743f836e554670c2
rani1029 <ranig1029@gmail.com>
2021-08-06 00:45:01 +0530

    Leap year program added

---
+++ LeapYear/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the year");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 == 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("year is leap year");
+            }
+            else
+                Console.WriteLine("year is not leap year");
+
+        }
+    }
+}
