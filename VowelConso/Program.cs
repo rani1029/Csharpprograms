6d58f9119ca8800315f1d6577012e9ab19b178e7
rani1029 <ranig1029@gmail.com>
2021-08-06 01:10:13 +0530

    VowelConso program added

---
+++ VowelConso/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace VowelConso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the alphabet to check");
+            char alpha = char.Parse(Console.ReadLine());
+            switch (alpha)
+            {
+                case 'a':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'A':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'E':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'I':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'O':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                case 'U':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+
+                default:
+                    Console.WriteLine("alphabet is consonent");
+                    break;
+
+
+            }
+        }
+    }
+}
