a22bd9530315c8c46a9c2dd355205ed1125dcb1e
rani1029 <ranig1029@gmail.com>
2021-08-05 16:49:57 +0530

    largest num program added

---
+++ Largestnum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Largestnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first num");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second num");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the third num");
+            int c = int.Parse(Console.ReadLine());
+            if (a > b && a > c)
+            {
+                Console.WriteLine("largest num is" + a);
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("largest num is" + b);
+            }
+            else
+                Console.WriteLine("largest num is  " + c);
+
+
+        }
+    }
+}
