eff7f144b7e81057ab33209a115192dc1f59e167
rani1029 <ranig1029@gmail.com>
2021-08-06 00:35:44 +0530

    Harmonic num program added

---
+++ Harmonic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Harmonic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the value N");
+            int n = int.Parse(Console.ReadLine());
+            float h = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                h += (float)1 / i;
+            }
+            Console.WriteLine("  Nth value of hatmonic number is " + " " + h);
+        }
+
+    }
+}
