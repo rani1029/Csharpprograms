17a4e08a7e93b6cf29d863d61cf0b0d616d243dc
rani1029 <ranig1029@gmail.com>
2021-08-05 11:35:25 +0530

    Even odd pragram added

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the no to check");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("number is even");
+            }
+            else
+                Console.WriteLine("number is odd");
+
+
+        }
+    }
+}
+
