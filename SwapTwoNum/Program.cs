f7903f0ba679f2b056df004b2fbfd2dd5c27de4c
rani1029 <ranig1029@gmail.com>
2021-08-05 11:30:10 +0530

    SwapTwoNum pragram added

---
+++ SwapTwoNum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwapTwoNum
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int c;
+            Console.WriteLine("enter first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            int b = int.Parse(Console.ReadLine());
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("no after swapping:" + a);
+            Console.WriteLine("no after swapping:" + b);
+
+
+
+
+        }
+    }
+}
