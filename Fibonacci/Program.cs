c813cf2e289330e91c78d83ede64245acf66a082
rani1029 <ranig1029@gmail.com>
2021-08-05 23:47:58 +0530

    fibonacci series added

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class fab
+    {
+        int n = 10;
+        int n1 = 0;
+        int n2 = 1;
+        static void Main(string[] args)
+        {
+
+            int n = 10;
+            int n1 = 0;
+            int n2 = 1;
+            Console.Write(n1 + " " + n2 + " ");
+            for (int i = 2; i <= n - 1; i++)
+            {
+                int n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                //Console.Write(i);
+                Console.Write(n3 + " ");
+            }
+
+        }
+    }
+}
