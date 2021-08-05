4773842c954b4f2757d274e24b64223b5d6d8c89
rani1029 <ranig1029@gmail.com>
2021-08-05 23:50:09 +0530

    reverse num program added

---
+++ ReverseNum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseNum
+{
+    class Reverse
+    {
+        static void Main(string[] args)
+        {
+            int Rev = 0;
+            int rem = 0;
+            Console.WriteLine("enter the number to reverse");
+            int num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                rem = num % 10;
+                Rev = Rev * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("number after reverse is :" + Rev);
+
+
+
+
+        }
+    }
+}
