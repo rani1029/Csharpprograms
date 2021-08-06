b4d60f9b8a24b7baef828fc51372c71f600a3d00
rani1029 <ranig1029@gmail.com>
2021-08-06 13:46:12 +0530

    PerFectNumber program added

---
+++ PerFectNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PerFectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("enter the number to check");
+            int num = int.Parse(Console.ReadLine());
+            int m = num / 2;
+            for (int i = 1; i <= m; i++)
+            {
+                int rem = num % i;
+                if (rem == 0)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                }
+                //else
+                //  break;
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("number is perfect number");
+            }
+            else
+                Console.WriteLine("number is not perfect number");
+
+
+        }
+    }
+}
